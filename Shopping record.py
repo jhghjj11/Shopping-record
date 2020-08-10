09f02c2ca59776827e2c32013c05c6a7fc79cd11
jhghjj11 <57930789+jhghjj11@users.noreply.github.com>
2020-08-10 18:03:15 +0800

    Add files via upload

---
+++ Shopping record.py
@@ -0,0 +1,17 @@
+product = []
+while True:
+    name = input("Please enter product's name: ")
+    if name == 'quit':
+        print('Good bye!')
+        break
+    price = input('Please enter the price: ')
+    if price == 'quit':
+        print('Good bye!')
+        break
+    #p = []
+    #p.append(name)
+    #p.append(price)
+    #p = [name, price]
+    #product.append(p)
+    product.append([name, price])
+print('Products storage: ', product)
