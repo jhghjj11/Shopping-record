bf564c83270c9fa5b187ea3858b6bdda2573d873
jhghjj11 <57930789+jhghjj11@users.noreply.github.com>
2020-08-16 13:24:00 +0800

    Add files via upload
    Refactored

--- Shopping record.py
+++ Shopping record.py
@@ -1,9 +1,8 @@
-import os
-product = []
 
-if os.path.isfile('products.csv'):
-    print('There is the file')
-    with open('products.csv', 'r', encoding = 'utf-8') as d:
+
+def read_file(filename):
+    product = []
+    with open(filename, 'r', encoding = 'utf-8') as d:
         for line in d:
             if '第一, 第二' in line:
                 continue
@@ -12,30 +11,49 @@ if os.path.isfile('products.csv'):
             #name = s[0]
             #price = s[1]
         print(product)
-else:
-    print('Not existing')
 
-while True:
-    name = input("Please enter product's name: ")
-    if name == 'quit':
-        print('Good bye!')
-        break
-    price = input('Please enter the price: ')
-    if price == 'quit':
-        print('Good bye!')
-        break
-    #p = []
-    #p.append(name)
-    #p.append(price)
-    #p = [name, price]
-    #product.append(p)
-    product.append([name, price])
-print('Products storage: ', product)
+    return product
 
-for things in product :
-    print(things[0], 'has a price at', things[1])
+def user_input(product):
+    while True:
+        name = input("Please enter product's name: ")
+        if name == 'quit':
+            print('Good bye!')
+            break
+        price = input('Please enter the price: ')
+        if price == 'quit':
+            print('Good bye!')
+            break
+        #p = []
+        #p.append(name)
+        #p.append(price)
+        #p = [name, price]
+        #product.append(p)
+        product.append([name, price])
+    print('Products storage: ', product)
+    return product
 
-with open('products.csv', 'w', encoding = 'utf-8') as f :
-    f.write('第一, 第二 \n')
-    for p in product:
-        f.write(p[0]  + ',' + str(p[1]) + '\n')
+def print_product(product):
+    for things in product :
+        print(things[0], 'has a price at', things[1])
+
+def write_file(filename, product):
+    with open(filename, 'w', encoding = 'utf-8') as f :
+        f.write('第一, 第二 \n')
+        for p in product:
+            f.write(p[0]  + ',' + str(p[1]) + '\n')
+
+def main():
+    import os
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('There is the file')
+        product = read_file(filename)
+    else:
+        print('Not existing')
+
+    product = user_input(product)
+    print_product(product)
+    write_file('products.csv', product)
+
+main()
